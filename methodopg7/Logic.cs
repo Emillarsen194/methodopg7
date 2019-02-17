d37aeeea7f74b42edea1688731aa1458c33ea2d9
Emillarsen194 <47022984+Emillarsen194@users.noreply.github.com>
2019-02-17 15:03:46 +0100

    Add project files.

---
+++ methodopg7/Logic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodopg7
+{
+    class Logic
+    {
+        public static int ArrayIndex() //make our array and return index of 5
+        {
+            int[] array = new int[9];
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i + 1;
+                if (array[i] == array[5])
+                {
+                    return array[5] * 2;
+                }
+            }
+
+            return array[5];
+        }
+    }
+}
